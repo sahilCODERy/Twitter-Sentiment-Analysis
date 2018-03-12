7469f54212c82dd63d27dfc7e10ddf873a0ae051
umangahuja1 <umangahuja1@gmail.com>
2018-03-12 20:05:04 +0530

    added politics data - 5000 tweets

--- app.py
+++ app.py
@@ -4,17 +4,13 @@ from textblob import TextBlob
 from tweepy import OAuthHandler
 import pandas as pd
 import os
-from keys import *
-
-directory = 'data'
-usr = 'SrBachchan'
-count = 3000
 
 consumer_key = 'MQzBTc1EU7khKXyGZSXzIXEwu'
 consumer_secret = 'ileULPagQp0Bmo0V5FS1KeHamMIsI8jQ6EdUlVmiTLQfjcx2sP'
 access_token = '711802845511614464-JdZ9gos2V1faZAXZLHRqAIkjrwU3ESE'
 access_secret = 'yTONtgIeVgPaiBHvIEArdrOPbERXur9Nnfz4w9sT1btHw'
 
+directory = 'data'
 
 if not os.path.exists(directory):
     os.makedirs(directory)
@@ -27,24 +23,33 @@ class Twitter(object):
             self.auth.set_access_token(access_token, access_secret)
             self.api = tweepy.API(self.auth)
         except:
-            print("Failed")
+            print("Authentication failed")
 
-    def clean_tweet(self, tweet):
+    def clean_helper(self, tweet):
         return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
 
-    def get_query_tweets(self, query, count=4):
-        tweets = []
-        try:
-            fetched_tweets = self.api.search(q=query, count=count, lang='en')
+    def clean_tweet(self, all_tweets):
+        cleaned_tweets = []
+        for tweet in all_tweets:
+            tweet.text = self.clean_helper(tweet.text)
+            cleaned_tweets.append(tweet)
+        return cleaned_tweets
 
-            for i, tweet in enumerate(fetched_tweets, 1):
-                print("{}. {}".format(i, tweet.text))
-                print("{}. {}".format(i, self.clean_tweet(tweet.text)))
-                tweet = self.clean_tweet(tweet.text)
-                tweets.append(tweet)
-        except tweepy.TweepError as e:
-            print("error : ", str(e))
-        return tweets
+    def get_topic_tweets(self, topic, count=100):
+        all_tweets = []
+        tweets = self.api.search(q=topic, count=count, lang='en')
+        count -= len(tweets)
+        all_tweets.extend(tweets)
+        last_id = tweets[-1].id - 1
+
+        while len(tweets) > 0 and count > 0:
+            tweets = self.api.search(q=topic, count=count, lang='en', max_id=last_id)
+            all_tweets.extend(tweets)
+            count -= len(tweets)
+            last_id = tweets[-1].id - 1
+
+        all_tweets = self.clean_tweet(all_tweets)
+        return all_tweets
 
     def get_user_tweet(self, name, count=5):
         all_tweets = []
@@ -60,6 +65,7 @@ class Twitter(object):
             count -= len(tweets)
             last_id = tweets[-1].id - 1
 
+        all_tweets = self.clean_tweet(all_tweets)
         return all_tweets
 
     def sentiment_analysis(self, tweets):
@@ -79,20 +85,51 @@ class Twitter(object):
         data = {'Sno': [], 'Tweet': [], 'Date': [], 'Likes': [], 'RTs': []}
         for index, tweet in enumerate(tweets, 1):
             data['Sno'].append(index)
-            data['Tweet'].append(self.clean_tweet(tweet.text))
+            data['Tweet'].append(self.clean_helper(tweet.text))
             data['Date'].append(tweet.created_at)
             data['Likes'].append(tweet.favorite_count)
             data['RTs'].append(tweet.retweet_count)
         data['Sentiments'] = self.sentiment_analysis(tweets)
         df = pd.DataFrame(data, columns=['Sno', 'Tweet', 'Date', 'Likes', 'RTs', 'Sentiments'])
 
+        df.dropna(inplace=True)
+        df.drop_duplicates(inplace=True)
+
         df.to_csv('{}/{}-{}.csv'.format(directory, usr, count), index=None)
+
+
+def user_wrapper(api):
+    usr = input('Enter username : ')
+    count = int(input('Enter number of tweets to be searched : '))
+
+    tweets = api.get_user_tweet(name=usr, count=count)
+    api.store_tweets(tweets, usr, count)
+
+
+def topic_wrapper(api):
+    topic = input('Enter topic to be searched : ')
+    count = int(input('Enter number of tweets to be searched : '))
+
+    tweets = api.get_topic_tweets(topic, count)
+    api.store_tweets(tweets, topic, count)
+
+
+def menu():
+    print('1. Search by username')
+    print('2. Search by topic')
+
+    choice = int(input('Enter your choice (1 or 2) : '))
+    return choice
 
 
 def main():
     api = Twitter()
-    tweets = api.get_user_tweet(name=usr, count=count)
-    api.store_tweets(tweets, usr, count)
+    choice = menu()
+
+    if (choice == 1):
+        user_wrapper(api)
+    if (choice == 2):
+        topic_wrapper(api)
 
 
 if __name__ == '__main__':
